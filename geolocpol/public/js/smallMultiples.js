3106fdd2b75d168fe30201f3b7832b8097c9d6d3
JulesSauvinet <julessauvinet@yahoo.fr>
2016-12-29 18:19:32 +0100

    scale ajusté avec la densité de population

--- geolocpol/public/js/smallMultiples.js
+++ geolocpol/public/js/smallMultiples.js
@@ -1,50 +1,87 @@
+//TODO PRENDRE EN COMPTE LA POPULATION OU LA SUPERFICIE DANS LES SCALES!
+
+//NOT USED mais eventuellement la taille de la vizu
 var width = 1550, height = 800;
 
-var curYear = '2014';
+//le polluant courant sélectionné
+var curPol = "NH3";
 
-var mapWidth = 250;
-var mapHeight = 200;
+//la taille d'une small map
+var mapWidth = 275;
+var mapHeight = 230;
 
-var projection = d3.geo.stereographic().center([3.9,43.0]).scale(300).translate([mapWidth / 2, mapHeight / 2]);
+//projection + path de l'europe d'une small map
+var projection = d3.geo.stereographic().center([3.9,43.0]).scale(350).translate([mapWidth / 2, mapHeight / 2]);
 
 var path = d3.geo.path()
     .projection(projection);
 
 var dateFormat = d3.time.format("%Y");
 
+//les années choisies (une map par année)
+//TODO more dynamic?
 var years = ["2000","2001","2002","2003","2004","2005","2006","2007",
              "2008","2009","2010","2011","2012","2013","2014"];
 
 
-function createPolData(pollutions){
-    var choice;
-    d3.selectAll(".radiopol").each(function(d){
-        rb = d3.select(this);
-        if(rb.property("checked")){
-            choice= rb.property("value");
-        }
+/*********************** NOT USED ********************************/
+var colorMap = {};
+var colors = [];
+//la scale de pollution
+var color0 = d3.scale.linear().range(['yellow', 'red']);
+//la scale de densité de pop
+var color1 = d3.scale.linear().range(['lightblue', 'darkblue']);
+//la scale de densité de pesticide
+var color2 = d3.scale.linear().range(['whitesmoke', 'orange']);
+//la scale d'energie
+var color3 = d3.scale.linear().range(['lightyellow', 'gold']);
+//la scale de nucleaire
+var color4 = d3.scale.linear().range(['yellow', 'brown']);
+//la scale des taxes
+var color5 = d3.scale.linear().range(['pink', 'magenta']);
+//la scale de mortalité infantile
+var color6 = d3.scale.linear().range(['pink', 'purple']);
+//la scale de transport
+var color7 = d3.scale.linear().range(['lightgreen', 'darkgreen']);
+//la scale de heartdisease
+var color8 = d3.scale.linear().range(['pink', 'purple']);
+//la scale de cancer
+var color9 = d3.scale.linear().range(['pink', 'purple']);
+//la scale de moteus
+var color10 = d3.scale.linear().range(['orange', 'brown']);
+colors.push(color0,color1,color2,color3,color4,color5,color6,color7,color8,color9,color10);
+/****************************************************************/
+
+//on dessine une map pour chaque année
+var dateJoin = d3.select('#maps').selectAll('div.map')
+    .data(years);
+
+var divs = dateJoin.enter()
+    .append('div').attr({
+        'id':function(d){ return 'map_'+d; },
+        'class':'map'
     });
-    var pollution = pollutions.filter(function(d,i){return (d.geo !== "EU28" && d.airpol === choice && d.airsect === "TOT_NAT")});
-    return pollution;
-}
+
+
+divs.append('p').text(function(d){ return dateFormat(new Date(d)); });
+
+//le titre -> nom de l'année
+var SVGs = divs.append('svg').attr({
+    'width':mapWidth,
+    'height':mapHeight
+});
+
+
 
 var pollutants = [];
-
-var color = d3.scale.linear().range(['yellow', 'red']);
-var color2 = d3.scale.linear().range(['lightgreen', 'darkgreen']);
-
-function init(error,pollutions,density, pesticides, energie, nuclear, taxes,
-              infantmort,transport, heartdiseases, cancer, motorcars, europe){
-
-    if (error) throw error;
-
+/* fonction pour créer le div des polluants de manière dynamique */
+function createPolDiv(pollutions){
     pollutions.forEach(function(p){
         if (!pollutants.includes(p.airpol)) {
             pollutants.push(p.airpol);
         }
     });
 
-    /********************* le div des polluants **********************/
     var fieldset = d3.select("#pollutiondiv").append("form");
     fieldset.append("legend").html("<h4>Choix polluant</h4>");
     var radioSpan = fieldset.selectAll(".radio").data(pollutants);
@@ -65,57 +102,180 @@ function init(error,pollutions,density, pesticides, energie, nuclear, taxes,
         });
     radioSpan.append("label")
         .html(function(d, i) {  return d.last == true ? d :  d + '<br>'});
+}
 
-
-    /****** On récupère la liste pollution en fonction du polluant (radio bouton) choisi *******/
-    var pollution = createPolData(pollutions);
-
-
-    //draw a map for each date
-    var dateJoin = d3.select('#maps').selectAll('div.map')
-        .data(years);
-
-    var divs = dateJoin.enter()
-        .append('div').attr({
-            'id':function(d){ return 'map_'+d; },
-            'class':'map'
-        });
-
-
-    divs.append('p').text(function(d){ return dateFormat(new Date(d)); });
-
-    var SVGs = divs.append('svg').attr({
-        'width':mapWidth,
-        'height':mapHeight
+var polMap = {};
+/* fonction qui créé les datasets des polluants pour chaque polluant*/
+function createPolDatas(pollutions){
+    pollutants.forEach(function(pollutant){
+        var pollution = pollutions.filter(function(d,i){return (d.geo !== "EU28" && d.airpol === pollutant && d.airsect === "TOT_NAT")});
+        polMap[pollutant] = pollution;
     });
+}
 
-    //on intègre les données de pollution aux données de map
+var dataMap = {};
+/* fonction pour un dataset contenant les données de map + du polluant courant sélectionné*/
+function createMergedPolAndMapData(curPol,europe){
+    var pollution = polMap[curPol];
     var data = topojson.feature(europe, europe.objects.regions).features;
 
     //création d'un tableau des codes nuts 1
     var geos = [];
 
-    pollution.forEach(function(p){
-        if (!geos.includes(p["geo"]))
-            geos.push(p["geo"]);
-    });
+    pollution.forEach(function(p){if (!geos.includes(p["geo"]))geos.push(p["geo"]);});
 
     data = data.filter(function(d,i){return geos.includes(d.properties["NUTS_ID"])});
-
 
     years.forEach(function(year){
         data.forEach(function(d){
             pollution.forEach(function(p){
                 if (d.properties["NUTS_ID"] === p["geo"]){
-                    d.properties[year] = parseFloat(p[year]);
+                    d.properties[year] = p[year];
                 }
+                if (!d.properties.dens)
+                    d.properties.dens = p["dens"];
             });
         });
     });
 
+    dataMap[curPol] = data;
+}
+
+var colorpol = {};
+/* fonction qui créé les scales de couleurs pour chaque polluant en fonction des min et max (appelé une seule fois) */
+function createScalesColorPol(){
+    pollutants.forEach(function(pollutant){
+        if (!colorpol[pollutant]){
+            pollution=polMap[pollutant];
+            var min = d3.min(pollution, function(pol) {
+                var mintmp = Number.MAX_VALUE;
+
+                for (var key in pol){
+                    //on ne base la scale que s'il y a une densité associé au code NUTS
+                    if (!isNaN(parseFloat(pol["dens"][key]))) {
+                        var value = parseFloat(pol[key]) / parseFloat(pol["dens"][key]);
+                        if (!isNaN(value)) {
+                            if (key !== "unit" && key !== "airsect" && key !== "geo" && key !== "airpol" && key !== "dens") {
+                                var year = parseInt(key);
+                                if (year >= 2000 && year <= 2014) {
+                                    if (parseFloat(pol[key]) !== 0 && value < parseFloat(mintmp)) {
+                                        mintmp = value;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return mintmp;
+            });
+            var max = d3.max(pollution, function(pol) {
+                var maxtmp = Number.MIN_VALUE;
+
+                for (var key in pol){
+                    //on ne base la scale que s'il y a une densité associé au code NUTS
+                    if (!isNaN(parseFloat(pol["dens"][key]))){
+                        var value = parseFloat(pol[key])/parseFloat(pol["dens"][key]);
+                        if (!isNaN(value)) {
+                            if (key !== "unit" && key !== "airsect" && key !== "geo" && key !== "airpol" && key !== "dens") {
+                                var year = parseInt(key);
+                                if (year >= 2000 && year <= 2014) {
+                                    if (value > parseFloat(maxtmp)) {
+                                        maxtmp = value;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return maxtmp;
+            });
+
+            var color = d3.scale.linear().range(['yellow', 'red']);
+            color.domain([min,max]);
+
+            colorpol[pollutant] = color;
+        }
+    });
+}
+
+function update(){
+
+}
+
+function updatePol(curPol){
+
+    var choice;
+    d3.selectAll(".radiopol").each(function(d){
+        rb = d3.select(this);
+        if(rb.property("checked")){
+            choice= rb.property("value");
+        }
+    });
+
+    data = dataMap[curPol];
 
     SVGs.each(function(date){
 
+        d3.select(this).selectAll('path')
+            .data(data)
+            .style("fill", function (d) {
+                if (!isNaN(d.properties[date])){
+                    //WATCHOUT Statique on remplace la densité de 2001 par celle de 2000
+                    // et celle de 2004 par celle de 2003 car données manquantes #empirisme
+                    var datebis = date;
+                    if (datebis === "2001")
+                        datebis = "2000";
+                    if (datebis === "2004")
+                        datebis = "2003";
+
+                    var value = (parseFloat(d.properties[date])/parseFloat(d.properties["dens"][datebis]));
+                    return colorpol[curPol](value);
+                }
+                else{
+                    console.log(d);
+                    return "lightgrey";
+                }
+            });
+    })
+}
+
+function init(error,pollutions,density, pesticides, energie, nuclear, taxes,
+              infantmort,transport, heartdiseases, cancer, motorcars, europe){
+
+    if (error) throw error;
+
+    //on intègre la données de densité au données de pollution pour calibrer les scales de couleurs notamment
+    density.forEach(function(dens){
+       pollutions.forEach(function(pol){
+           if (!pol['dens'])
+               pol['dens']={};
+           if (pol["geo"] === dens["geo"]){
+               years.forEach(function(year){
+                   pol['dens'][year]= dens[year];
+               });
+           }
+       });
+    });
+
+    //on créé le div des polluants
+    createPolDiv(pollutions);
+
+    //On récupère la liste pollution en fonction du polluant (radio bouton) choisi
+    createPolDatas(pollutions);
+
+    //on intègre les données de pollution aux données de map
+    pollutants.forEach(function(pol){
+        createMergedPolAndMapData(pol,europe);
+    });
+
+    //on créé les scales de couleurs pour chaque polluants
+    createScalesColorPol();
+
+    //les données courantes
+    var data = dataMap[curPol];
+
+
+    SVGs.each(function(date){
         d3.select(this).selectAll('path')
             .data(data)
             .enter().append("path")
@@ -127,41 +287,16 @@ function init(error,pollutions,density, pesticides, energie, nuclear, taxes,
             });
     });
 
-    var test=0;
-    function update(){
-        SVGs.each(function(date){
 
-            var dataRange = [];
-            pollution.forEach(function (p) {
-                if (p["geo"] !== 'EU28'){
-                    dataRange.push(p[date]);
-                }
-            });
+    d3.selectAll("input[type=radio][name=pol]")
+        .on("change", function() {
+            var elem = d3.select(this);
+            curPol = elem.property("value");
+            updatePol(curPol);
+        });
 
-            var min = d3.min(dataRange, function(d) { if (parseFloat(d) !== 0.0) return parseFloat(d);});
-            var max = d3.max(dataRange, function(d) { if (parseFloat(d) !== 0.0) return parseFloat(d);});
-            color.domain([min,max]);
-
-            d3.select(this).selectAll('path')
-                .data(data)
-                .style("fill", function (d) {
-                    if (test === 0){
-                        console.log(d.properties[date]);
-                        test++;
-                    }
-
-                    if (!isNaN(d.properties[date])){
-                        //console.log(color(d.properties[date]));
-                        return color(d.properties[date]);
-                    }
-                    /*else{
-                        console.log(d);
-                        return "lightgrey";
-                    }*/
-                });
-        })
-    }
-
+    updatePol(curPol);
+    //on update la vizu
     update();
 }
 
